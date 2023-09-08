6580604214744b8eba7c133cda18cddae49ccb90
RubenSmn <saaman.ruben@gmail.com>
2023-09-08 14:18:41 +0200

    chore: add fetch from web to server

--- apps/server/src/app.controller.spec.ts
+++ apps/server/src/app.controller.spec.ts
@@ -15,8 +15,9 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello from the server"', () => {
+      const data = appController.getHello();
+      expect(data).toMatchObject({ message: 'Hello from the server' });
     });
   });
 });
