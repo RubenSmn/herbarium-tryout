597974626c5d05407d5132c4e3eab2ed11e709a3
RubenSmn <saaman.ruben@gmail.com>
2023-09-09 12:25:04 +0200

    chore: remove useless change to test ci cache

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,8 +2,6 @@ export default async function Home() {
   const response = await fetch("http://localhost:4000");
   const data = await response.json();
 
-  // useless change for linting
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <p>{data.message}</p>
